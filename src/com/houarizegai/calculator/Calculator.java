a6bcee4b29ebe5b67a84486e21fea7947a78d8c7
Ranjith CS <Ranjith.CS@Ltts.com>
2020-09-30 16:25:24 +0530

    Anil changes

--- src/com/houarizegai/calculator/Calculator.java
+++ src/com/houarizegai/calculator/Calculator.java
@@ -23,6 +23,8 @@ private static final int MARGIN_mI = 20;
     private static final int MARGIN_mN = 60;
 private static final int Akash1 = 20;
     private static final int Akash2 = 60;
+private static final int Anil1 = 20;
+    private static final int Anil2 = 60;
 
 
     private JFrame window; // Main window
